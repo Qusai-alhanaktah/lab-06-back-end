90a5740f3c1f19d9d0d7c1b4927f76866df2ec1a
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-12-22 21:52:05 +0200

    finished the weather issue

--- server.js
+++ server.js
@@ -5,64 +5,54 @@ const server = express();
 require('dotenv').config();
 
 const cors = require('cors');
- server.use(cors());
+server.use(cors());
 
- const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3500;
 
- server.listen(PORT , ()=>{
-     console.log('its work');
- })
+server.listen(PORT, () => {
+    console.log('its work');
+})
 
-server.get('/' , (request ,response) =>{
+server.get('/', (request, response) => {
     response.status(200).send('Okay its found');
 });
-server.get('/location', (request, response)=>{
+// ///////////////////////////
+server.get('/location', (request, response) => {
     const locationData = require('./data/geo.json')
     const location = new Location(locationData)
     response.status(200).send(location)
 })
-
-server.get('/weather' , (request , response)=>{
+// //////////////////////
+server.get('/weather', (request, response) => {
     const weatherData = require('./data/darksky.json');
-        for (let i = 0; i < weatherData.daily.data.length; i++) {
-            let date = new Date(weatherData.daily.data[i].time * 1000).toString()
-            let forecast =weatherData.daily.data[i].summary;
-            new Weather(forecast,date);
-
-        }
-            response.status(200).send( Weather.all);
+    for (let i = 0; i < weatherData.daily.data.length; i++) {
+        let date = new Date(weatherData.daily.data[i].time * 1000).toString()
+        let forecast = weatherData.daily.data[i].summary;
+        new Weather(forecast, date);
+    }
+    response.status(200).send(Weather.all);
 })
-
-
-
-
-server.use('*' , (request , response) =>{
+// ///////////////////////////
+server.use('*', (request, response) => {
     response.status(404).send('its not found ')
 });
-server.use((error ,request , response) =>{
+// //////////////////////////////////
+server.use((error, request, response) => {
     response.status(500).send("Sorry, something went wrong");
 });
 
-
-// {
-//     "search_query": "seattle",
-//     "formatted_query": "Seattle, WA, USA",
-//     "latitude": "47.606210",
-//     "longitude": "-122.332071"
-//   }
-
 // consturtor function location
-function Location (data){
-    this.formatted_query= data[0].display_name;
-    this.latitude= data[0].lat
-    this.longitude= data[0].lon
-    
-} 
-// consturtor function weather
-function Weather (forecast,date){
-        this.forecast = forecast;
-        this.time =date;
-        Weather.all.push(this);
-    console.log( Weather.all); 
+function Location(data) {
+    this.formatted_query = data[0].display_name;
+    this.latitude = data[0].lat
+    this.longitude = data[0].lon
+
 }
-Weather.all=[]
+// consturtor function weather
+function Weather(forecast, date) {
+    this.forecast = forecast;
+    this.time = date;
+    Weather.all.push(this);
+    console.log(Weather.all);
+}
+Weather.all = []
